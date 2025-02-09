e2aaf93605bdca5bc33cc20d87af38c37fe74dcc
yoo-jaehee <uujhee@gmail.com>
2025-02-09 14:12:37 +0900

    나이 증감 예제

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
-function App() {
+function UserInfo() {
+  const [name, setName] = useState("에밀리");
+  const [age, setAge] = useState(20);
+
   return (
-    <h1>yooMaster's code</h1>
+    <div>
+      <h2>이름: {name}</h2>
+      <h2>나이: {age}</h2>
+      <p>
+        <button onClick={() => setAge(age + 1)}>나이 증가</button>
+      </p>
+      <button onClick={() => setAge(age - 1)}>나이 감소</button>
+    </div>
   );
 }
 
-export default App;
+export default UserInfo;
